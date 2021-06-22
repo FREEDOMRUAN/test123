10d798deb941d995120b9315508d24ac11564ce3
Liu RUAN <ruanliu@ocac-suzhou.net>
2021-06-22 14:01:25 +0800

    test123

---
+++ 1.js
@@ -0,0 +1,14 @@
+// const stus = ['a','b','c','d'];
+
+
+// for (const stu in stus) {
+//     console.log(stu);
+// }
+
+
+
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let res = nums.filter((num) => { console.log(num) });
+
+//console.log(res);
